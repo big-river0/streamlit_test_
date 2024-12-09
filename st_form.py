4db673d6130dc2eb269a734997ad01e51cddbc15
Big_River <122954757+big-river0@users.noreply.github.com>
2024-12-09 10:57:53 +0900

    Add files via upload

--- st_form.py
+++ st_form.py
@@ -10,8 +10,8 @@ with st.form('my_form'):
 
     # 입력 위젯
     coffee_bean_val = st.selectbox('메인 메뉴', ['돈까스', '제육','파스타','카레돈까스','쫄면','치즈돈까스'])
-    coffee_roast_val = st.selectbox('서브 메뉴', ['샐러드', '치킨 샐러드'])
-    brewing_val = st.selectbox('음료', ['콜라', '사이다', '레몬에이드', '복숭아 아이스티'])
+    coffee_roast_val = st.selectbox('서브 메뉴', ['없음','샐러드', '치킨 샐러드'])
+    brewing_val = st.selectbox('음료', ['없음','콜라', '사이다', '레몬에이드', '복숭아 아이스티'])
     serving_type_val = st.selectbox('추가 주문', ['없음','밥 추가', '미니 쫄면', '미니 샐러드'])
 
     # 모든 양식은 제출 버튼을 가져야 함
